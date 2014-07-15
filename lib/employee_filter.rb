fa67dc90883e265277ca93f0b3dd97d7fe2f826a
Spencer Eldred <eldredspencer@gmail.com>
2014-07-15 08:57:01 -0600

    One possible solution from Spencer

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -1,3 +1,27 @@
 class EmployeeFilter
+  require 'date'
+
+  def initialize(employees)
+    @employees = employees
+  end
+
+  def started_before_2006
+    result = []
+    @employees.each { |employee| result << employee if employee[:start_date].year < 2006 }
+    result
+  end
+
+
+  def all_with_start_date
+    result = []
+    @employees.each do |employee|
+      if employee[:start_date]
+        result << "#{employee[:first_name]} #{employee[:last_name]} (#{employee[:title]}) - #{employee[:start_date].month}/#{employee[:start_date].day}/#{employee[:start_date].year}"
+      end
+    end
+    result
+  end
+
+
 
 end
