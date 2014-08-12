f311052a6751d34997eba778042435ab302528f4
EmilyPlatzer <emilyplatzer@gmail.com>
2014-08-11 22:30:58 -0600

    User can get a list of employees with start date, list of employees who started before 2006

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -1,3 +1,18 @@
 class EmployeeFilter
 
+  def initialize(employees_data)
+    @employees_data = employees_data
+  end
+
+  def started_before_2006
+    @employees_data.select {|employee| employee[:start_date] < Date.new(2006,1,1)}
+  end
+
+  def all_with_start_date
+    @employees_data.map do |employee|
+      start_date = employee[:start_date].strftime('%-m/%-d/%Y')
+      "#{employee[:first_name]} #{employee[:last_name]} (#{employee[:title]}) - #{start_date}"
+    end
+  end
+
 end
